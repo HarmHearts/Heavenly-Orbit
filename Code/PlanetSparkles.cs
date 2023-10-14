dea114eed57bc65669fe0a5f7dc3caae0c5dcc7c
nil <nillinvanillin@gmail.com>
2023-10-13 23:27:54 -0400

    I DID IT
    more particle

--- Code/PlanetSparkles.cs
+++ Code/PlanetSparkles.cs
@@ -3,6 +3,14 @@ using System;
 
 public partial class PlanetSparkles : GpuParticles2D
 {
+	[Export]
+	private float speedInfluence;
+	[Export]
+	private float baseSpeed;
+	[Export]
+	private float speedVariance;
+	[Export]
+	private bool bodyToggle;
 	private ParticleProcessMaterial particle;
 	private Player player;
 	// Called when the node enters the scene tree for the first time.
@@ -16,5 +24,8 @@ public partial class PlanetSparkles : GpuParticles2D
 	public override void _Process(double delta)
 	{
 		particle.Direction = new Vector3(player.MoveDirection.X, player.MoveDirection.Y, 0);
-	}
+		particle.InitialVelocityMax = baseSpeed + speedVariance + (player.MoveSpeedNormalized * speedInfluence);
+        particle.InitialVelocityMin = baseSpeed + (player.MoveSpeedNormalized * speedInfluence);
+		this.Emitting = (!player.Locked || player.LockedBody != bodyToggle);
+    }
 }
